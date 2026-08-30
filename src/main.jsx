3e5a493e2bac94b82e235bdd07e81015f0487acd
SaifUrRehman2k <surgamer2007@gmail.com>
2026-08-30 23:12:47 +0500

    this is stupid_4

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Portfolio_React">
     <App/>
   </BrowserRouter>,
 )
